7c9bb98d43e6e104b7ecae1c9b463780d86e828b
daniel.olaneta.farina <daniel.olaneta.farina@udc.es>
2021-03-02 13:41:01 +0100

    Corregido null LeerFichero

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ package com.company;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -20,13 +21,12 @@ public class Main {
         }
     }
 
-    private static String [] lecturaLineasArchivo(String [] args) {
+    private static ArrayList<String> lecturaLineasArchivo(String [] args) {
         File archivo = null;
         FileReader fr = null;
         BufferedReader br = null;
         String rutaArchivo = "";
-        String [] lineasArchivo = null;
-        int i = 0;
+        ArrayList<String> lineasArchivo = new ArrayList<String>();
         System.out.println(args[0]);
         if (args.length == 0)
             System.exit(0);
@@ -43,8 +43,7 @@ public class Main {
                 String linea;
                 while ((linea = br.readLine()) != null){
                     System.out.println("Linea: " + linea);
-                    lineasArchivo[i] = linea;
-                    i = i++;
+                    lineasArchivo.add(linea);
                 }
 
             } catch (Exception e) {
@@ -67,7 +66,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        String[] lineasFichero = lecturaLineasArchivo(args);
+        ArrayList<String> lineasFichero = lecturaLineasArchivo(args);
         int pointer = -1;
         System.out.println(lecturaLineasArchivo(args));
         //crearArbol(lineasFichero[0].split(" "), pointer);
