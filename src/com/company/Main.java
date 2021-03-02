6a0de178944496ef43243c48f917a25d1957ae4f
angel.alvarez.rey <angel.alvarez.rey@udc.es>
2021-03-02 13:13:52 +0100

    No encuentra el archivo de ejemplo (lady3doors.txt)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,24 @@
 package com.company;
-import java.io.*;
 
 
 public class Main {
 
+    private static void crearArbol(String[] lineasFichero, int pointer){
+        String r = lineasFichero[pointer++];
+
+        if (r.equals("&") || r.equals("|") || r.equals(">") || r.equals("=") || r.equals("%") ){
+            System.out.println(r + " MACAQUINHO " + pointer);
+            crearArbol(lineasFichero, pointer);
+        } else if(r.equals("-") || r.equals("0") || r.equals("1")){
+            return;
+        } else {
+            return;
+        }
+    }
+
     public static void main(String[] args) {
-
-    String[] lineasFichero = LeerFichero.lecturaLineasArchivo(args);
-
+        String[] lineasFichero = LeerFichero.lecturaLineasArchivo(args);
+        int pointer = -1;
+        crearArbol(lineasFichero[0].split(" "), pointer);
     }
 }
